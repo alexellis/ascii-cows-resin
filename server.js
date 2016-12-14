097468449c90725551746de2eee9e03e2eb5a83d
Alex Ellis <alexellis2@gmail.com>
2016-12-14 12:12:53 +0000

    Convert to es6

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var app = express();
-var cows = require('cows')
-var all = cows();
+"use strict"
 
-function show(cow) {
+let express = require('express');
+let app = express();
+let cows = require('cows')
+let all = cows();
+
+let show = (cow) => {
     var cowText = all[cow];
     return cowText;
 }
 
-// reply to request with "Hello World!"
 app.get('/', function (req, res) {
   var randomCow = Math.floor((Math.random() * all.length) + 1);
   if(req.headers.accept &&
@@ -20,10 +21,7 @@ app.get('/', function (req, res) {
   }
 });
 
-//start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
-
   var port = server.address().port;
-  console.log('Example app listening on port ', port);
-
+  console.log('Listening on port ', port);
 });
